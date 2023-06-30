3902270c9d0d266f304ba1ce49aae4496231a560
Sahil Bhatia <sahilbhatiag@gmail.com>
2023-07-01 00:33:17 +0530

    3rd commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,30 @@
 <template>
     <div>
-        <h2>ShopApp</h2>
-        <p>Welcome to Shop-app, your go-to destination for a seamless and enjoyable online shopping experience. Discover a wide range of products from various categories, all conveniently accessible at your fingertips.</p>
+        <h1>ShopApp</h1>
+        <p>Welcome to Shop-app, your go-to destination for a seamless and enjoyable online shopping experience. Discover a wide range of products from various categories, all conveniently accessible at your fingertips.Welcome to Shop-app, your go-to destination for a seamless and enjoyable online shopping experience. Discover a wide range of products from various categories, all conveniently accessible at your fingertips.Welcome to Shop-app, your go-to destination for a seamless and enjoyable online shopping experience. Discover a wide range of products from various categories, all conveniently accessible at your fingertips.</p>
+        <br>
         <br>
         <h3>Explore our Product Catalog</h3>
-        <p>Browse through our extensive collection of products, ranging from fashion, electronics, home decor, beauty, and much more. With our intuitive user interface, finding the perfect item has never been easier.</p>
+        <p>Browse through our extensive collection of products, ranging from fashion, electronics, home decor, beauty, and much more. With our intuitive user interface, finding the perfect item has never been easier.Browse through our extensive collection of products, ranging from fashion, electronics, home decor, beauty, and much more. With our intuitive user interface, finding the perfect item has never been easier.Browse through our extensive collection of products, ranging from fashion, electronics, home decor, beauty, and much more. With our intuitive user interface, finding the perfect item has never been easier.</p>
+        <br>
         <br>
         <h3>Personalize Your Shopping Experience</h3>
-        <p>Customize your browsing experience by utilizing our advanced filtering options. Sort products by price, popularity, or category to quickly find what you're looking for. Our responsive design ensures a seamless experience, whether you're shopping on your computer, tablet, or mobile device.</p>
+        <p>Customize your browsing experience by utilizing our advanced filtering options. Sort products by price, popularity, or category to quickly find what you're looking for. Our responsive design ensures a seamless experience, whether you're shopping on your computer, tablet, or mobile device.Customize your browsing experience by utilizing our advanced filtering options. Sort products by price, popularity, or category to quickly find what you're looking for. Our responsive design ensures a seamless experience, whether you're shopping on your computer, tablet, or mobile device.Customize your browsing experience by utilizing our advanced filtering options. Sort products by price, popularity, or category to quickly find what you're looking for. Our responsive design ensures a seamless experience, whether you're shopping on your computer, tablet, or mobile device.</p>
+        <br>
         <br>
         <h3>Detailed Product Information</h3>
-        <p>Get comprehensive details about each product, including descriptions, specifications, customer reviews, and ratings. Make informed purchasing decisions based on real user feedback and recommendations.</p>
+        <p>Get comprehensive details about each product, including descriptions, specifications, customer reviews, and ratings. Make informed purchasing decisions based on real user feedback and recommendations.Get comprehensive details about each product, including descriptions, specifications, customer reviews, and ratings. Make informed purchasing decisions based on real user feedback and recommendations.Get comprehensive details about each product, including descriptions, specifications, customer reviews, and ratings. Make informed purchasing decisions based on real user feedback and recommendations.</p>
+        <br>
         <br>
         <h3>Stay Updated with the Latest Trends</h3>
-        <p>Never miss out on the latest product releases, exclusive offers, and discounts. Sign up for our newsletter to receive regular updates directly in your inbox. Stay ahead of the curve and be the first to explore new arrivals and limited-time promotions.</p>
+        <p>Never miss out on the latest product releases, exclusive offers, and discounts. Sign up for our newsletter to receive regular updates directly in your inbox. Stay ahead of the curve and be the first to explore new arrivals and limited-time promotions.Never miss out on the latest product releases, exclusive offers, and discounts. Sign up for our newsletter to receive regular updates directly in your inbox. Stay ahead of the curve and be the first to explore new arrivals and limited-time promotions.Never miss out on the latest product releases, exclusive offers, and discounts. Sign up for our newsletter to receive regular updates directly in your inbox. Stay ahead of the curve and be the first to explore new arrivals and limited-time promotions.</p>
+        <br>
         <br>
         <h3>Join our Community</h3>
         <p>Connect with fellow shoppers, share your experiences, and engage in discussions on our vibrant community forums. Get valuable insights, seek recommendations, and be part of a community that shares your passion for shopping.
 
         Shop-app aims to deliver an exceptional online shopping experience, combining a user-friendly interface, a vast product selection, and top-notch customer service. Start exploring our catalog today and discover the joy of hassle-free online shopping.</p>
+        <br>
         <br>
     </div>
 </template>
@@ -28,9 +34,9 @@
 </script>
 
 <style scoped>
-    h2 {
+    h1 {
         margin-bottom: 30px;
-        font-size: 40px;
+        font-size: 60px;
     }
     h3 {
         margin-bottom: 30px;
